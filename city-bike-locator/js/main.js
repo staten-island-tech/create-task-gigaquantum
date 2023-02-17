73acca07124b291ae78416798b69e0fb226664e9
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2023-02-16 20:52:30 -0500

    Update main.js

--- city-bike-locator/js/main.js
+++ city-bike-locator/js/main.js
@@ -1,20 +1,26 @@
-function sineSquared(x) {
-  return (1 - Math.cos(2 * x)) / 2;
-}
-
 function coordinateDistanceCalc(latCoordA, longCoordA, latCoordB, longCoordB) {
-  let radius = 6371; // Average Radius of the Earth (in Kilometers)
+  // Average Radius of the Earth (in Kilometers)
+  const radius = 6371; 
+
+  // Converting the coordinates to Radians
+  latCoordA = latCoordA * (Math.PI / 180);
+  latCoordB = latCoordB * (Math.PI / 180);
+  longCoordA = longCoordA * (Math.PI / 180);
+  longCoordB = longCoordB * (Math.PI / 180);
+  
+  // Haversine Formula
   const distance =
     2 *
     radius *
     Math.asin(
       Math.sqrt(
-        sineSquared((latCoordB - latCoordA) / 2) +
-          Math.cos(latCoordA) *
+        (Math.sin((latCoordB - latCoordA) / 2) ** 2) +
+          (Math.cos(latCoordA) *
             Math.cos(latCoordB) *
-            sineSquared((longCoordB - longCoordA) / 2)
+            (Math.sin((longCoordB - longCoordA) / 2) ** 2))
       )
-    ); // Haversine Formula: Math is incorrect. Must fix math.
+    );
+  
   return distance;
 }
 
@@ -27,15 +33,6 @@ const apiData = apiFunctions.fetchAPI("http://api.citybik.es/v2/networks");
 
 const userLatitude = 40.731672;
 const userLongitude = -73.977477;
-
-console.log(
-  coordinateDistanceCalc(
-    40.6892748550208,
-    -74.04457957845808,
-    40.748441410666224,
-    -73.98566372649938
-  )
-);
 
 apiData.then((data) => {
   console.log(data.networks);
@@ -50,10 +47,10 @@ apiData.then((data) => {
       console.log(element);
     }
   });
-  const sortedArray = data.networks.sort(
+  const formattedArray = data.networks.sort(
     (a, b) => a.location.userDistance - b.location.userDistance
-  );
-  console.log(sortedArray);
+  ).filter(bikeStation => bikeStation.location.userDistance <= searchRadius)
+  console.log(formattedArray);
 });
 
 function addCard(distance, latitude, longitude, name, city, brand) {
