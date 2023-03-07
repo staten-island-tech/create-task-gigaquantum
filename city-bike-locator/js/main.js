053f893a6fced7eb3779cf24dc2dc510651592a3
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2023-03-06 19:10:40 -0500

    Update main.js

--- city-bike-locator/js/main.js
+++ city-bike-locator/js/main.js
@@ -68,11 +68,13 @@ DOMSelectors.submitBtn.addEventListener("click", function () {
         );
       });
 
+      let apiCallArray = [];
       if (
         data.networks.filter(
           (element) =>
             element.location.userDistance <= userData.searchRadius + 50
-          /* Adds 50KM so if the center of the Bike Station Network is farther than the closest Bike station in that network, the program will still list it.*/
+          /* Adds 50KM so if the center of the Bike Station Network is farther 
+          than the closest Bike station in that network, the program will still list it.*/
         ).length == 0
       ) {
         alert(
@@ -82,7 +84,6 @@ DOMSelectors.submitBtn.addEventListener("click", function () {
         DOMSelectors.resetBtn.disabled = false;
         return;
       } else {
-        let apiCallArray = [];
         data.networks
           .filter(
             (element) =>
